f7c109304aacb8d07c7a0144825c4ec18ef9d24f
lisyolang <lisyolang@naver.com>
2015-03-29 18:16:00 +0900

    Revert "Revert "Update icebox.js""
    This reverts commit 9d12991deaa61fa85295774ea4bd37160363a355.

--- node/myIceBoxServer/routes/icebox.js
+++ node/myIceBoxServer/routes/icebox.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET json result. */
 //http://localhost:3000/icebox
 router.get('/', function(req, res) {
-	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Content-Type', 'application/json; charset=utf-8');
 	var sampleJson = { icebox : [
 					{id : 10000, desc : "my ice box", material : "apple egg juice"},
 					{id : 10003, desc : "your ice box", material : "cake chicken"}]}
@@ -16,10 +16,14 @@ router.get('/recipe', function(req, res) {
 	var item = req.query.item;
 	console.log(item)
 	
-	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Content-Type', 'application/json; charset=utf-8');
 	if (item == "계란") {
 		var recipeJson = { recipelist : [
-					{title : "계란말이", items : "계란 파 치즈", recipe : "#1 계란을 그릇에 푼다. #2 파를 잘게 썰어서 넣는다. #3 치즈를 넣고 섞는다. #4 후라이팬에 잘 익힌다."}]}	res.end(JSON.stringify(recipeJson));
+					{title : "계란말이", 
+					items : "계란 파 치즈", 
+					recipe : "#1 계란을 그릇에 푼다. #2 파를 잘게 썰어서 넣는다. #3 치즈를 넣고 섞는다. #4 후라이팬에 잘 익힌다."
+					}]
+				}
 		res.end(JSON.stringify(recipeJson));
 	}
 });
